9c0a34d61d8b037a2e7c300d743be4a8dcd45bc9
Kaiz_JP <kaiz@kaiz.jp>
2020-12-25 18:22:22 +0900

    0.4

--- src/main/java/jp/kaiz/rtmdengoadapter/RTMDengoAdapter.java
+++ src/main/java/jp/kaiz/rtmdengoadapter/RTMDengoAdapter.java
@@ -15,7 +15,7 @@ import org.lwjgl.LWJGLException;
 public class RTMDengoAdapter {
     public static final String MODID = "rtmdengoadapter";
     public static final String NAME = "RTM Dengo Adapter";
-    public static final String VERSION = "1.7.10_0.3";
+    public static final String VERSION = "1.7.10_0.4";
 
     public static boolean ATSA_LOADED;
 
